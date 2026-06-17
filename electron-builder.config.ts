7dc16f319763d0856ab42d7623ae7377bab20982
rolex <rolex@MacBook-Air.local>
2026-06-17 11:13:54 +0800

    update app icon assets

--- electron-builder.config.ts
+++ electron-builder.config.ts
@@ -1,13 +1,21 @@
 import type { Configuration } from 'electron-builder'
 
 const config: Configuration = {
-  appId: 'com.oraca.markdown',
-  productName: 'Oraca',
+  appId: 'com.orca.markdown',
+  productName: 'orca',
   mac: {
     category: 'public.app-category.productivity',
+    icon: 'build/icon.icns',
     target: [{ target: 'dmg', arch: ['arm64', 'x64'] }],
   },
+  win: {
+    icon: 'build/icon.ico',
+  },
+  linux: {
+    icon: 'build/icon.png',
+  },
   directories: {
+    buildResources: 'build',
     output: 'release',
   },
 }
